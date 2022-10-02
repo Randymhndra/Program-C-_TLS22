5d01645ca01c781d976fdbfeb6974abaf3d0083e
Randymhndra <114851123+Randymhndra@users.noreply.github.com>
2022-10-02 20:50:45 +0700

    Add files via upload

---
+++ Money Converter.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float idr;
+    int usd = 15303;
+    int eur = 15000;
+    int bp  = 17064;
+    int jpy = 106;
+    int sgd = 10687;
+    
+    cout << "Masukkan nilai uang dalam IDR : Rp ";
+    cin >> idr;
+    cout << "Berikut adalah hasil konversi mata uang Rupiah" << endl;
+    cout << "Dolar Amerika   : $ "  << idr*usd << endl;
+    cout << "Euro            : € "  << idr*eur << endl;
+    cout << "Poundsterling   : £ "  << idr*bp  << endl;
+    cout << "Yen             : ¥ "  << idr*jpy << endl;
+    cout << "Dolar Singapura : S$ " << idr*sgd << endl;
+    
+    return 0;
+}
